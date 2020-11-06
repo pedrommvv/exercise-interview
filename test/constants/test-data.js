2d7b0bfabf7fc88ca4d572098179dbddafbf45f2
pedrommvv <pedro.ferreira@talkdesk.com>
2020-11-06 21:55:40 +0000

    Add test data constants
    Data that will be used during the test execution

---
+++ test/constants/test-data.js
@@ -0,0 +1,41 @@
+export const MAIN_MENU = {
+  news: [
+    'shopnav-flyout-collection-items-45501694-41db-48f5-beff-77e6547a3132',
+    {
+      modelCars:
+        'shopnav-flyout-collection-items-a8c38b84-dd59-4a6b-afeb-81e222ecc9ad',
+      fashionBeauty:
+        'shopnav-flyout-collection-items-77a7b92d-61f8-4292-a546-82f3bf40d608',
+      watches:
+        'shopnav-flyout-collection-items-408529e5-1a28-45e0-a6a1-28aabaa55d9b',
+      home:
+        'shopnav-flyout-collection-items-d0e7ef98-31e8-414f-b91a-26ef8aeaeb8e',
+      outdoor:
+        'shopnav-flyout-collection-items-9e2dcd32-4316-4b8b-b4d9-1ec6e4f0cbd7',
+      children:
+        'shopnav-flyout-collection-items-77a7b92d-61f8-4292-a546-82f3bf40d608'
+    }
+  ],
+  modelCars:
+    'shopnav-flyout-collection-items-4e1b636f-6c87-4610-af94-706beee4c86f',
+  fashionBeauty:
+    'shopnav-flyout-collection-items-953f137a-3663-4a76-baee-75c3a37e3e4c',
+  watches: [
+    'shopnav-flyout-collection-items-c37978e0-8ce2-45c0-a302-9b7360f55213',
+    {
+      women:
+        'shopnav-flyout-collection-items-48e75769-9ab5-4be4-aba7-66a672c96775',
+      men:
+        'shopnav-flyout-collection-items-d48a1ba9-4bcb-4c3b-b24b-d7a11be04251',
+      children:
+        'shopnav-flyout-collection-items-0839b71f-2b95-418f-b439-4d3319663358',
+      other:
+        'shopnav-flyout-collection-items-8c3f4708-c13e-45dc-8c27-a4bd6fcb84e0'
+    }
+  ],
+  home: 'shopnav-flyout-collection-items-c4425d72-3450-49d7-b3e5-07140afaa442',
+  outdoor:
+    'shopnav-flyout-collection-items-50065c59-1961-4497-ae0a-7ba5da04eeba'
+}
+export const QUANTITY_INPUT = '2'
+export const GUEST_EMAIL = 'sopas@gmail.com'
